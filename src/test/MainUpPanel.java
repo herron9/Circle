da89ce4aa6715361693f9ace8c0c9b94df0239c7
herron9 <yanghrong@outlook.com>
2015-10-09 14:03:02 -0400

    project initialization

---
+++ src/test/MainUpPanel.java
@@ -0,0 +1,41 @@
+//package test;
+//
+//import javax.swing.JFrame;
+//import javax.swing.JPanel;
+//import java.awt.CardLayout;
+//import java.awt.Color;
+//
+//public class MainUpPanel extends JPanel {
+//
+//	public static CardLayout MainpageCl = new CardLayout();
+//	public static MainChatPanel MCPanel = new MainChatPanel();
+//	public static ChattingPanel Chatting = new ChattingPanel();
+//	
+//	public MainUpPanel() {
+//		setBackground(new Color(51, 204, 204));
+//		setLayout(MainpageCl);
+//		//setBounds(0,0,600,400);
+//		add(MCPanel,"MCPanel");
+//		add(Chatting,"Chatting");
+////		MainpageCl.show(MCPanel,"MCPanel");
+//		
+//
+//
+//	}
+//	
+//	
+//	public CLayout() {
+//		panelCont.setLayout(cl);
+//		panelCont.add("Log",panelLog);
+//		panelCont.add("Reg",panelReg);
+//		panelCont.add("Main",panelMain);
+//
+//		cl.show(panelCont, "Main");
+//		
+//		Circle.getContentPane().add(panelCont);
+//		Circle.setBounds(100, 100, 600, 470);
+//		Circle.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+//		Circle.setResizable(false);
+//	}
+//
+//}
