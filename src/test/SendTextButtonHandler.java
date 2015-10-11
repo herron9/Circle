3b4cb3349804236f87fa48fd7650ff61000ec657
Ziqi Zhang <root@ip184-178-33-152.ga.at.cox.net>
2015-10-11 11:47:09 -0400

    friends

--- src/test/SendTextButtonHandler.java
+++ src/test/SendTextButtonHandler.java
@@ -1,25 +1,54 @@
 package test;
 
+<<<<<<< HEAD
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+=======
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import client.CircleClient;
+import communication.Message;
+
+>>>>>>> 5c557e8270e871b1166aedc595ef8ee8057f627d
 public class SendTextButtonHandler implements ActionListener{
 
 	JTextField MsgField;
 	JTextArea ChatArea;
+<<<<<<< HEAD
 	
 	public SendTextButtonHandler(JTextField MsgField, JTextArea ChatArea) {
 		// TODO Auto-generated constructor stub
 		this.MsgField = MsgField;
 		this.ChatArea = ChatArea;
+=======
+	CircleClient client;
+	
+	public SendTextButtonHandler(JTextArea ChatArea,JTextField MsgField, CircleClient client) {
+		// TODO Auto-generated constructor stub
+		this.MsgField = MsgField;
+		this.ChatArea = ChatArea;
+		this.client = client;
+>>>>>>> 5c557e8270e871b1166aedc595ef8ee8057f627d
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent ae)
     {
+<<<<<<< HEAD
    	 if(MsgField.getText().equals("")){}
    	 else{
    		 System.out.println(MsgField.getText());
@@ -30,3 +59,38 @@ public class SendTextButtonHandler implements ActionListener{
     }
 
 }
+=======
+
+		ArrayList<String> des = new ArrayList<>();
+	    des.add("duras.yhr@gmail.com");
+		//des.add("yanghrong@outlook.com");
+//		Message message = new Message(Message.TEXT, des, "huang zhi", "test message", new Date().toString());
+		Message message = Message.builder()
+				.messageType(Message.TEXT)
+				.messageSrcID(LoginPanel.circleAccessToken)
+				.messageDesIDList(des)
+				.messageContent(MsgField.getText())
+				.messageTimeStamp(new Date().toString())
+				.build();
+		try {
+			client.sendTextMessage(message);
+			//ChatArea.setForeground(new Color(51, 153, 102));//blue
+			ChatArea.append("-"+message.getMessageSrcID()+"-");
+			//ChatArea.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+			ChatArea.append(" "+message.getMessageTimeStamp()+"\n");
+			//ChatArea.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+			ChatArea.append(message.getMessageContent()+"\n");
+			//JOptionPane.showMessageDialog(null,"-"+message.getMessageSrcID()+"-"+message.getMessageContent());
+			//ChatArea.setForeground(new Color(0, 153, 255));
+			//ChatArea.append(MsgField.getText());
+			MsgField.setText("");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+
+}
+
+>>>>>>> 5c557e8270e871b1166aedc595ef8ee8057f627d
